1d3e85af0e803d53a646792b66eb502cc58ef96c
sleepingkingstudios <sleeping.king.studios@gmail.com>
2014-02-07 18:05:03 -0500

    Implement remaining RolesController actions.
    Current spec status: 29 examples, 0 failures in 0.38035 seconds.

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,16 +1,72 @@
 # app/controllers/roles_controller.rb
 
 class RolesController < ApplicationController
+  before_action :build_resource, :only => %i(new create)
+  before_action :load_resource,  :only => %i(show edit update destroy)
   before_action :load_resources, :only => %i(index)
 
+  # POST /roles
+  def create
+    if @role.save
+      redirect_to @role
+    else
+      render :new
+    end # if-else
+  end # method create
+
+  # DELETE /roles/:id
+  def destroy
+    @role.destroy
+
+    redirect_to roles_path
+  end # method destroy
+
+  # GET /roles/:id/edit
+  def edit; end
+
   # GET /roles
-  def index
-    
-  end # action index
+  def index; end
+
+  # GET /roles/new
+  def new; end
+
+  # GET /roles/:id
+  def show; end
+
+  # PATCH /roles/:id
+  def update
+    if @role.update_attributes role_params
+      redirect_to @role
+    else
+      render :edit
+    end # if-else
+  end # action update
 
   private
+
+  def build_resource
+    @role = Role.new role_params
+  end # method build_resource
+
+  def load_resource
+    @role = Role.find params[:id]
+  end # method load_resource
 
   def load_resources
     @roles = Role.all
   end # method load_resources
+
+  def process_date! hsh
+    if hsh.has_key? 'date_submitted(1i)'
+      hsh[:date_submitted] = Date.new(*[*1..3].map { |index|
+        hsh.delete("date_submitted(#{index}i)").to_i
+      }) # end date
+    end # if
+    hsh
+  end # process_date!
+
+  def role_params
+    hsh = params.fetch(:role, {}).permit(:company, :title, :date_submitted, :url, :notes)
+    process_date!(hsh)
+  end # method role_params
 end # class
--- spec/controllers/roles_controller_spec.rb
+++ spec/controllers/roles_controller_spec.rb
@@ -4,10 +4,147 @@ require 'spec_helper'
 
 RSpec.describe RolesController do
   describe '#index' do
-    it 'responds with 200 ok and renders the show template' do
+    it 'responds with 200 ok and renders the index template' do
       get :index
       expect(response.status).to be == 200
       expect(response).to render_template 'index'
+    end # it
+  end # describe
+
+  describe '#new' do
+    it 'responds with 200 ok and renders the new template' do
+      get :new
+      expect(response.status).to be == 200
+      expect(response).to render_template 'new'
+    end # it
+  end # describe
+
+  describe '#create' do
+    context 'with an invalid role' do
+      let(:params) { { :role => {} } }
+
+      def perform_action
+        post :create, params
+      end # method perform_action
+
+      it 'responds with 200 ok and renders the new template' do
+        perform_action
+        expect(response.status).to be == 200
+        expect(response).to render_template 'new'
+      end # it
+
+      it 'does not create a new role' do
+        expect { perform_action }.not_to change(Role, :count)
+      end # it
+    end # context
+
+    context 'with a valid role' do
+      let(:params) { { :role => FactoryGirl.attributes_for(:role) } }
+      let(:role)   { Role.last }
+
+      def perform_action
+        post :create, params
+      end # method perform_action
+
+      it 'redirects to show' do
+        perform_action
+        expect(response.status).to be == 302
+        expect(response).to redirect_to role_path(role)
+      end # it
+
+      it 'creates a new role' do
+        expect { perform_action }.to change(Role, :count).to(1)
+      end # it
+    end # context
+  end # describe
+
+  describe '#show' do
+    let(:params) { { :id => role.id } }
+    let!(:role) { FactoryGirl.create :role }
+
+    it 'responds with 200 ok and renders the show template' do
+      get :show, params
+      expect(response.status).to be == 200
+      expect(response).to render_template 'show'
+    end # it
+  end # describe
+
+  describe '#edit' do
+    let(:params) { { :id => role.id } }
+    let!(:role) { FactoryGirl.create :role }
+
+    it 'responds with 200 ok and renders the edit template' do
+      get :edit, params
+      expect(response.status).to be == 200
+      expect(response).to render_template 'edit'
+    end # it
+  end # describe
+
+  describe '#update' do
+    let(:params) { { :id => role.id } }
+    let!(:role) { FactoryGirl.create :role }
+
+    context 'with invalid attributes' do
+      let(:attributes) { { :title => nil } }
+      let(:params) { super().merge :role => attributes }
+
+      def perform_action
+        patch :update, params
+      end # method perform_action
+
+      it 'responds with 200 ok and renders the edit template' do
+        perform_action
+        expect(response.status).to be == 200
+        expect(response).to render_template 'edit'
+      end # it
+
+      it 'does not update the persisted role' do
+        expect {
+          perform_action
+          role.reload
+        }.not_to change(role, :title)
+      end # it
+    end # context
+
+    context 'with valid attributes' do
+      let(:attributes) { { :title => 'New Title' } }
+      let(:params) { super().merge :role => attributes }
+
+      def perform_action
+        patch :update, params
+      end # method perform_action
+
+      it 'redirects to show' do
+        perform_action
+        expect(response.status).to be == 302
+        expect(response).to redirect_to role_path(role)
+      end # it
+
+      it 'updates the persisted role' do
+        expect {
+          perform_action
+          role.reload
+        }.to change(role, :title).to(attributes[:title])
+      end # it
+    end # context
+  end # describe
+
+  describe '#destroy' do
+    let(:params) { { :id => role.id } }
+    let!(:role) { FactoryGirl.create :role }
+
+    def perform_action
+      delete :destroy, params
+    end # method perform_action
+
+    it 'redirects to index' do
+      perform_action
+      expect(response.status).to be == 302
+      expect(response).to redirect_to roles_path
+    end # it
+
+    it 'destroys the role' do
+      expect { perform_action }.to change(Role, :count).to(0)
     end # it
   end # describe
 end # describe
