1791a1a2f41b14a24d7b696098d5644f0d7cbe4b
sleepingkingstudios <sleeping.king.studios@gmail.com>
2014-02-07 11:21:06 -0500

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.2'
+
+### App Server ###
+# gem 'unicorn'
+
+### Assets ###
+gem 'haml-rails',        '~> 0.4'
+gem 'sass-rails',        '~> 4.0.0'
+gem 'coffee-rails',      '~> 4.0.0'
+gem 'jquery-rails',      '~> 3.0.4'
+gem 'uglifier',          '>= 1.3.0' # Compressor for JavaScript assets
+
+### Documentation ###
+group :doc do
+  # bundle exec rake doc:rails generates the API under doc/api.
+  gem 'sdoc', require: false
+end # end group
+
+ruby "2.1.0"
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Drone::Application.config.secret_key_base = '4997ef579dd58c981f511a1791fbc3906d28137c91436b7b1b766680b71e0327c1cb2b8ad92e0f0324fbda3205349fb7fb47d03fc0d424417465792e6b40d291'
