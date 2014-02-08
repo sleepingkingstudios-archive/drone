93cd7986fac7842d83111e7184485d351b44e68b
sleepingkingstudios <sleeping.king.studios@gmail.com>
2014-02-07 19:19:05 -0500

    Add RecruitersController#new action.
    Current spec status: 44 examples, 0 failures in 0.20105 seconds.

--- app/controllers/recruiters_controller.rb
+++ app/controllers/recruiters_controller.rb
@@ -1,14 +1,26 @@
 # app/controllers/recruiters_controller.rb
 
 class RecruitersController < ApplicationController
+  before_action :build_resource, :only => %i(new)
   before_action :load_resources, :only => %i(index)
 
   # GET /recruiters
   def index; end
 
+  # GET /recruiters/new
+  def new; end
+
   private
+
+  def build_resource
+    @recruiter = Recruiter.new recruiter_params
+  end # method build_resource
 
   def load_resources
     @recruiters = Recruiter.all
   end # method load_resources
+
+  def recruiter_params
+    params.fetch(:recruiter, {}).permit(:name, :agency, :email, :phone_number)
+  end # method recruiter_params
 end # class
--- spec/controllers/recruiters_controller_spec.rb
+++ spec/controllers/recruiters_controller_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe RecruitersController do
       expect(response).to render_template 'index'
     end # it
   end # describe
+
+  describe '#new' do
+    it 'responds with 200 ok and renders the new template' do
+      get :new
+      expect(response.status).to be == 200
+      expect(response).to render_template 'new'
+    end # it
+  end # describe
 end # describe
